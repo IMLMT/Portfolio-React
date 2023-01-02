299e30bed66ee0310250fde08439b3574ebef215
lthomas97 <lthomas97@gatech.edu>
2023-01-02 15:59:55 -0600

    heroku

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3001;
 
 // const db = require("./models")
 
-
 // Define middleware here 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
